13d51914e046445ea5cc10741df9e42138f5e66d
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-03-13 13:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Your Script here.
+function rot13(str) {
+  var decodedStr = "";
+  for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      // A-Z range
+      var decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+      decodedStr += String.fromCharCode(decodedCharCode);
+    } else {
+      decodedStr += str[i];
+    }
+  }
+  return decodedStr;
+}
